8ebea17b8ae6e690136b990e655f8669277a9623
instrctt <nsfwlord26fork@tutamail.com>
2026-07-19 17:00:14 -0400

    feature: added qs to the exec file.

--- cli/editor/nvim/lua/hum/plugins/telescope.lua
+++ cli/editor/nvim/lua/hum/plugins/telescope.lua
@@ -21,7 +21,7 @@ return {
     },
     pickers = {
 	find_files = {
-		hidden = true,
+		hidden = false,
 	},
     },
     extensions = {},
--- cli/terminal/fish/config.fish
+++ cli/terminal/fish/config.fish
@@ -1,2 +1,8 @@
-# Remove default greeting
-#set -g fish_greeting
+if status is-interactive
+    if not set -q SSH_AGENT_PID; or not kill -0 $SSH_AGENT_PID 2>/dev/null
+        eval (ssh-agent -c | grep -v ^echo)
+        set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+        set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+        ssh-add ~/.ssh/id_ed25519_instrctt 2>/dev/null
+    end
+end
